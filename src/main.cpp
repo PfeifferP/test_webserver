d5ac312561a130d69ab8f0425064938570d0d460
PfeifferP <admin@pfeiffer-privat.de>
2022-07-08 05:46:50 +0200

    geändert auf LittleFS

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
-#include <ArduinoOTA.h>
+
 #ifdef ESP32
 #include <FS.h>
-#include <SPIFFS.h>
+#include <LittleFS.h>
 #include <ESPmDNS.h>
 #include <WiFi.h>
 #include <AsyncTCP.h>
@@ -16,80 +16,6 @@
 
 // SKETCH BEGIN
 AsyncWebServer server(80);
-AsyncWebSocket ws("/ws");
-AsyncEventSource events("/events");
-
-void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
-  if(type == WS_EVT_CONNECT){
-    Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
-    client->printf("Hello Client %u :)", client->id());
-    client->ping();
-  } else if(type == WS_EVT_DISCONNECT){
-    Serial.printf("ws[%s][%u] disconnect\n", server->url(), client->id());
-  } else if(type == WS_EVT_ERROR){
-    Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
-  } else if(type == WS_EVT_PONG){
-    Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len)?(char*)data:"");
-  } else if(type == WS_EVT_DATA){
-    AwsFrameInfo * info = (AwsFrameInfo*)arg;
-    String msg = "";
-    if(info->final && info->index == 0 && info->len == len){
-      //the whole message is in a single frame and we got all of it's data
-      Serial.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT)?"text":"binary", info->len);
-
-      if(info->opcode == WS_TEXT){
-        for(size_t i=0; i < info->len; i++) {
-          msg += (char) data[i];
-        }
-      } else {
-        char buff[3];
-        for(size_t i=0; i < info->len; i++) {
-          sprintf(buff, "%02x ", (uint8_t) data[i]);
-          msg += buff ;
-        }
-      }
-      Serial.printf("%s\n",msg.c_str());
-
-      if(info->opcode == WS_TEXT)
-        client->text("I got your text message");
-      else
-        client->binary("I got your binary message");
-    } else {
-      //message is comprised of multiple frames or the frame is split into multiple packets
-      if(info->index == 0){
-        if(info->num == 0)
-          Serial.printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
-        Serial.printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
-      }
-
-      Serial.printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT)?"text":"binary", info->index, info->index + len);
-
-      if(info->opcode == WS_TEXT){
-        for(size_t i=0; i < len; i++) {
-          msg += (char) data[i];
-        }
-      } else {
-        char buff[3];
-        for(size_t i=0; i < len; i++) {
-          sprintf(buff, "%02x ", (uint8_t) data[i]);
-          msg += buff ;
-        }
-      }
-      Serial.printf("%s\n",msg.c_str());
-
-      if((info->index + len) == info->len){
-        Serial.printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
-        if(info->final){
-          Serial.printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
-          if(info->message_opcode == WS_TEXT)
-            client->text("I got your text message");
-          else
-            client->binary("I got your binary message");
-        }
-      }
-    }
-  }
-}
 
 
 const char* ssid = "*******";
@@ -97,7 +23,14 @@ const char* password = "*******";
 const char * hostName = "esp-async";
 const char* http_username = "admin";
 const char* http_password = "admin";
+//
+const int buf_size = 32; 
+char s[buf_size];
 
+void printDirectory(File dir,int numTabs = 3);
+
+
+//
 void setup(){
   Serial.begin(115200);
   Serial.setDebugOutput(true);
@@ -111,38 +44,21 @@ void setup(){
     WiFi.begin(ssid, password);
   }
 
-  //Send OTA events to the browser
-  ArduinoOTA.onStart([]() { events.send("Update Start", "ota"); });
-  ArduinoOTA.onEnd([]() { events.send("Update End", "ota"); });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    char p[32];
-    sprintf(p, "Progress: %u%%\n", (progress/(total/100)));
-    events.send(p, "ota");
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    if(error == OTA_AUTH_ERROR) events.send("Auth Failed", "ota");
-    else if(error == OTA_BEGIN_ERROR) events.send("Begin Failed", "ota");
-    else if(error == OTA_CONNECT_ERROR) events.send("Connect Failed", "ota");
-    else if(error == OTA_RECEIVE_ERROR) events.send("Recieve Failed", "ota");
-    else if(error == OTA_END_ERROR) events.send("End Failed", "ota");
-  });
-  ArduinoOTA.setHostname(hostName);
-  ArduinoOTA.begin();
+  
 
   MDNS.addService("http","tcp",80);
 
-  SPIFFS.begin();
+  Serial.print(F("Initializing FS..."));
+    if (LittleFS.begin()){
+        Serial.println(F("done."));
+    }else{
+        Serial.println(F("fail."));
+    }
 
-  ws.onEvent(onWsEvent);
-  server.addHandler(&ws);
-
-  events.onConnect([](AsyncEventSourceClient *client){
-    client->send("hello!",NULL,millis(),1000);
-  });
-  server.addHandler(&events);
+  
 
 #ifdef ESP32
-  server.addHandler(new SPIFFSEditor(SPIFFS, http_username,http_password));
+  server.addHandler(new SPIFFSEditor(LittleFS, http_username,http_password));
 #elif defined(ESP8266)
   server.addHandler(new SPIFFSEditor(http_username,http_password));
 #endif
@@ -151,7 +67,7 @@ void setup(){
     request->send(200, "text/plain", String(ESP.getFreeHeap()));
   });
 
-  server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.htm");
+  server.serveStatic("/", LittleFS, "/").setDefaultFile("index.htm");
 
   server.onNotFound([](AsyncWebServerRequest *request){
     Serial.printf("NOT_FOUND: ");
@@ -214,9 +130,59 @@ void setup(){
       Serial.printf("BodyEnd: %u\n", total);
   });
   server.begin();
+
+  // To format all space in LittleFS
+    // LittleFS.format()
+ 
+    // Get all information of your LittleFS
+ 
+    unsigned int totalBytes = LittleFS.totalBytes();
+    unsigned int usedBytes = LittleFS.usedBytes();
+    unsigned int freeBytes;
+ 
+    Serial.println("File system info:");
+
+    snprintf(s, buf_size, "\tTotal: %10d bytes", totalBytes);
+    puts(s);
+ 
+    snprintf(s, buf_size, "\tUsed: %11d bytes", usedBytes);
+    puts(s);
+
+    freeBytes = totalBytes - usedBytes;
+    snprintf(s, buf_size, "\tFree: %11d bytes", freeBytes);
+    puts(s);
+ 
+    Serial.println();
+ 
+    // Open dir folder
+    File dir = LittleFS.open("/");
+    // Cycle all the content
+    printDirectory(dir);
 }
 
 void loop(){
-  ArduinoOTA.handle();
-  ws.cleanupClients();
+  
+}
+
+void printDirectory(File dir, int numTabs) {
+    while (true) {
+        File entry = dir.openNextFile();
+        if (! entry) {
+            // no more files
+            break;
+        }
+        for (uint8_t i = 0; i < numTabs; i++) {
+          Serial.print('\t');
+        }
+        Serial.print(entry.name());
+        if (entry.isDirectory()) {
+          Serial.println("/");
+          printDirectory(entry, numTabs + 1);
+        } else {
+          // files have sizes, directories do not
+          Serial.print("\t\t");
+          Serial.println(entry.size(), DEC);
+        }
+        entry.close();
+    }
 }
